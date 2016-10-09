2d49f633db9f6d1192703e9f814f3bf65dff7544
liqiang <liqiang@dd6d2e1d-415a-4aae-9b9e-7302682725ca>
2016-10-09 08:09:57 +0000

    Ios core changes.

--- module/video_capture/main/source/iOS/ioscapture.mm
+++ module/video_capture/main/source/iOS/ioscapture.mm
@@ -17,6 +17,7 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 #import "ioscapture.h"
+#import <pthread.h>
 #ifdef TARGET_OS_IPHONE
 #if 0
 char *globalFilePathcapture = NULL;
