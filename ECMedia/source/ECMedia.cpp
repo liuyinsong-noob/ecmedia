01aa6953e302a1230df06858aa25d94604355504
liqiang <liqiang@dd6d2e1d-415a-4aae-9b9e-7302682725ca>
2016-12-07 02:20:24 +0000

    Update media lib version No. to "2.1.1".

--- ECMedia/source/ECMedia.cpp
+++ ECMedia/source/ECMedia.cpp
@@ -91,7 +91,7 @@ static int m_cameraCount = 0;
 using namespace cloopenwebrtc;
 using namespace std;
 
-#define ECMEDIA_VERSION "2.1.0"
+#define ECMEDIA_VERSION "2.1.1"
 
 //extern bool g_media_TraceFlag;
 //void PrintConsole(const char * fmt,...){};
