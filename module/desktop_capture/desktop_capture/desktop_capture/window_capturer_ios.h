cc3f8ea4488a97701a1cfc55e50ffc5b295ec43f
liqiang <liqiang@dd6d2e1d-415a-4aae-9b9e-7302682725ca>
2016-12-07 02:25:52 +0000

    Ios screen share.

---
+++ module/desktop_capture/desktop_capture/desktop_capture/window_capturer_ios.h
@@ -0,0 +1,45 @@
+//
+//  window_capturer_ios.h
+//  desktop_capture
+//
+//  Created by SeanLee on 2016/11/24.
+//  Copyright © 2016年 SeanLee. All rights reserved.
+//
+
+#ifndef window_capturer_ios_h
+#define window_capturer_ios_h
+namespace cloopenwebrtc {
+    class ScreenCapturerIos : public ScreenCapturer {
+    public:
+        ScreenCapturerIos();
+        virtual ~ScreenCapturerIos();
+        
+        // WindowCapturer interface.
+        bool GetShareCaptureRect(int &width, int &height);
+        
+        // DesktopCapturer interface.
+        void Start(Callback* callback) ;
+        void Capture(const DesktopRegion& region) ;
+        
+        virtual bool GetScreenList(ScreenList* screens);
+        
+        // Select the screen to be captured. Returns false in case of a failure (e.g.
+        // if there is no screen with the specified id). If this is never called, the
+        // full desktop is captured.
+        virtual bool SelectScreen(ScreenId id);
+        
+        
+    private:
+        bool IsAeroEnabled();
+        
+        Callback* callback_;
+        
+        
+        DesktopSize previous_size_;
+        
+        DISALLOW_COPY_AND_ASSIGN(ScreenCapturerIos);
+    };
+}
+#endif /* window_capturer_io
+
+s_h */
