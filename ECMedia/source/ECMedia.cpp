289b18ae9d75c1fabe9db1d3d6e1f2028019935f
leixb <leixb@dd6d2e1d-415a-4aae-9b9e-7302682725ca>
2017-02-13 02:32:02 +0000

    Version 2.1.2.13

--- ECMedia/source/ECMedia.cpp
+++ ECMedia/source/ECMedia.cpp
@@ -133,7 +133,7 @@ static int m_cameraCount = 0;
 using namespace cloopenwebrtc;
 using namespace std;
 
-#define ECMEDIA_VERSION "2.1.2.12"
+#define ECMEDIA_VERSION "2.1.2.13"
 
 //extern bool g_media_TraceFlag;
 //void PrintConsole(const char * fmt,...){};
