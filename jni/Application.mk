eceaabfb85aad6a47139324cc8766468f007a7b0
gezhaoyou <gezhaoyou@126.com>
2017-11-07 18:08:52 +0800

    modify android lib support x64

--- jni/Application.mk
+++ jni/Application.mk
@@ -4,7 +4,7 @@ APP_PLATFORM := android-21
 APP_STL := gnustl_static #stlport_static #gnustl_static#
 #APP_ABI := armeabi-v7a
 #APP_ABI := armeabi arm64-v8a x86_64
-APP_ABI := armeabi #arm64-v8a #x86 x86_64
+APP_ABI := armeabi arm64-v8a #x86 x86_64
 APP_CPPFLAGS :=  -std=c++11 -fexceptions
 APP_CFLAGS :=-Wno-psabi \
 	-fno-exceptions \
