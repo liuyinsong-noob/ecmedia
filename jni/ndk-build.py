2d286c96cc3a76e5444371bde50acf77c3025c97
gezhaoyou <gezhaoyou@126.com>
2018-03-07 14:21:00 +0800

    chore: add ndk-build.py, can run 'ndk-build' and copy libs to android demo

---
+++ jni/ndk-build.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import os
+import sys
+import time
+
+class BuildBase:
+    def __init__(self):
+        self.libsPath = os.path.join(os.getcwd(), '..', 'libs/.')
+        self.appLibPath = os.path.join(os.getcwd(), '..', 'demo_Android/CCP_Demo_v3.6.4r/libs')
+        print self.libsPath
+        print self.appLibPath
+
+    def run(self):
+        self.build()
+        self.copyLibsToApp()
+
+    def build(self):
+        print os.system('ndk-build -j 4')
+    
+    def copyLibsToApp(self):
+        print self.libsPath
+        print os.system('cp -r ' + self.libsPath + ' ' + self.appLibPath)
+
+if __name__=='__main__' :
+    buildAndroid = BuildBase()
+    buildAndroid.run()
