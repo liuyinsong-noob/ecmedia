6968e4ef6c35112fa25166d6688cff8dba456ee4
gezhaoyou <gezhaoyou@126.com>
2018-04-17 10:29:28 +0800

    version: 2.3.1.3

--- ECMedia/source/ECMedia.cpp
+++ ECMedia/source/ECMedia.cpp
@@ -166,7 +166,7 @@ static int m_cameraCount = 0;
 using namespace cloopenwebrtc;
 using namespace std;
 
-#define ECMEDIA_VERSION "ecmedia_version: v2.3.1.2"
+#define ECMEDIA_VERSION "ecmedia_version: v2.3.1.3"
 
 //extern bool g_media_TraceFlag;
 //void PrintConsole(const char * fmt,...){};
