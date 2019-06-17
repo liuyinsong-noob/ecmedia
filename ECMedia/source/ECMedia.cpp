4a8ae76c4e938820e897daabe53b3841da7def46
葛昭友 <gezhaoyou@126.com>
2019-06-17 10:11:28 +0800

    [version]: v2.3.3.5

--- ECMedia/source/ECMedia.cpp
+++ ECMedia/source/ECMedia.cpp
@@ -78,7 +78,7 @@ CurlPost *g_curlpost = nullptr;
 #endif
 
 
-#define ECMEDIA_VERSION "ecmedia_version: v2.3.3.4"
+#define ECMEDIA_VERSION "ecmedia_version: v2.3.3.5"
 
 enum {
     ERR_SDK_ALREADY_INIT = -1000,
