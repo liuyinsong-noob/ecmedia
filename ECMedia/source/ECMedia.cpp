426fed6480d5f6e5faf23530c69659c70b039787
葛昭友 <gezhaoyou@126.com>
2019-07-04 17:35:28 +0800

    [bug-fix] 减小视频发送码率

--- ECMedia/source/ECMedia.cpp
+++ ECMedia/source/ECMedia.cpp
@@ -3705,9 +3705,9 @@ static void ECMedia_reset_send_codecinfo(VideoCodec& videoCodec)
       switch (scale)
       {
         case 4://360p
-          videoCodec.maxBitrate = 550;
+          videoCodec.maxBitrate = 500;
           videoCodec.minBitrate = 30;
-          videoCodec.startBitrate = 400;
+          videoCodec.startBitrate = 350;
           break;
         case 8://720p
           videoCodec.maxBitrate = 1100;
@@ -3715,9 +3715,9 @@ static void ECMedia_reset_send_codecinfo(VideoCodec& videoCodec)
           videoCodec.startBitrate = 900;
           break;
         default:
-          videoCodec.maxBitrate = 550;
+          videoCodec.maxBitrate = 500;
           videoCodec.minBitrate = 30;
-          videoCodec.startBitrate = 400;
+          videoCodec.startBitrate = 350;
         break;
       }
     }
