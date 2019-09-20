3ed86691972f5d991a93067cc46452868f96aab4
yuanlr <yuanlr@yuntongxun.com>
2019-09-20 14:13:30 +0800

    update version v2.3.3.13

--- ECMedia/source/ECMedia.cpp
+++ ECMedia/source/ECMedia.cpp
@@ -80,7 +80,7 @@ CurlPost *g_curlpost = nullptr;
 #endif
 
 
-#define ECMEDIA_VERSION "ecmedia_version: v2.3.3.12"
+#define ECMEDIA_VERSION "ecmedia_version: v2.3.3.13"
 
 enum {
     ERR_SDK_ALREADY_INIT = -1000,
