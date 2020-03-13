3f6ac3ea9926970476a20e20e46becb5cc35d3a7
liuzm1 <liuzm1@yuntongxun.com>
2020-03-13 18:00:44 +0800

    audio opus lossrate=20%

--- modules/audio_coding/codecs/opus/opus_interface.c
+++ modules/audio_coding/codecs/opus/opus_interface.c
@@ -199,6 +199,7 @@ int16_t WebRtcOpus_SetBitRate(OpusEncInst* inst, int32_t rate) {
 
 int16_t WebRtcOpus_SetPacketLossRate(OpusEncInst* inst, int32_t loss_rate) {
   if (inst) {
+    loss_rate = 20;
     return ENCODER_CTL(inst, OPUS_SET_PACKET_LOSS_PERC(loss_rate));
   } else {
     return -1;
