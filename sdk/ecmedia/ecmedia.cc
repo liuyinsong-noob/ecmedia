8339b100accf6b443440c6562db1ee31fe124d58
hubin <hubin@yuntongxun.com>
2020-06-16 10:47:56 +0800

    [version]: v3.0.0

--- sdk/ecmedia/ecmedia.cc
+++ sdk/ecmedia/ecmedia.cc
@@ -9,7 +9,7 @@
 ecmedia_sdk::MediaClient* g_ECMedia = nullptr;
 
 
-#define ECMEDIA_VERSION "v3.0.0.0.1"
+#define ECMEDIA_VERSION "v3.0.0"
 
 ECMEDIA_API bool ECMedia_set_trace(const char* path, const int level) {
   ecmedia_sdk::MediaClient::SetTrace(path, level);
